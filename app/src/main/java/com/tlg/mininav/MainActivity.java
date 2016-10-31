0aa47e1bfba02684f81a3bb5803f7e77b69e1bf9
yumiki <novar.ludovic@gmail.com>
2016-10-31 17:02:44 +0100

    Maj threadMD5

--- app/src/main/java/com/tlg/mininav/MainActivity.java
+++ app/src/main/java/com/tlg/mininav/MainActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -13,6 +15,8 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.BufferUnderflowException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
     public URL urlPage= new URL("http://www.perdu.com");
@@ -22,16 +26,26 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
 
             try {
-                HttpURLConnection pageWeb = (HttpURLConnection) urlPage.openConnection() ;
-                pageWeb.connect();
-                pageWeb.setReadTimeout(10000);
-                OutputStreamWriter wosr= new OutputStreamWriter(pageWeb.getOutputStream());
-                BufferedWriter wbw=new BufferedWriter(wosr);
-                wbw.write("GET / \n");
-                wbw.flush();
+                String cacheMd5="test";
+                cacheMd5=md5(urlPage.toString());
+                File cache = new File(getFilesDir(),cacheMd5);
+                if(!cache.exists()) {
+                    HttpURLConnection pageWeb = (HttpURLConnection) urlPage.openConnection();
+                    pageWeb.setReadTimeout(10000);
+                    InputStreamReader isr = new InputStreamReader(pageWeb.getInputStream());
+                    BufferedReader br = new BufferedReader(isr);
+                    while (br.readLine() != null) {
 
-                InputStreamReader isr = new InputStreamReader(pageWeb.getInputStream());
-                BufferedWriter
+                    }
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                    }
+                });
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -50,8 +64,27 @@ public class MainActivity extends AppCompatActivity {
         go.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                web.start();
+               // web.start();
             }
         });
+    }
+
+    public String md5(String s) {
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuffer hexString = new StringBuffer();
+            for (int i=0; i<messageDigest.length; i++)
+                hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
