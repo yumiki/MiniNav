5ed15d506529fed7418afe392ac5cdcf34a1e797
yumiki <novar.ludovic@gmail.com>
2016-10-31 15:09:36 +0100

    Ajout Thread

--- app/src/main/java/com/tlg/mininav/MainActivity.java
+++ app/src/main/java/com/tlg/mininav/MainActivity.java
@@ -3,8 +3,34 @@ package com.tlg.mininav;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+public class MainActivity extends AppCompatActivity {
+    URL urlPage= new URL("");
+
+    Thread web = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            HttpURLConnection pageWeb = new HttpURLConnection(urlPage) {
+                @Override
+                public void disconnect() {
+
+                }
+
+                @Override
+                public boolean usingProxy() {
+                    return false;
+                }
+
+                @Override
+                public void connect() throws IOException {
+
+                }
+            }
+        }
+    });
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
