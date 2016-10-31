6c1ed857685b869100a8a5861c39cfbf18999e9b
yumiki <novar.ludovic@gmail.com>
2016-10-31 17:09:30 +0100

    Fonction MD5

--- app/src/main/java/com/tlg/mininav/MainActivity.java
+++ app/src/main/java/com/tlg/mininav/MainActivity.java
@@ -18,6 +18,8 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.BufferUnderflowException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
     public URL urlPage= new URL("http://www.perdu.com");
@@ -73,5 +75,23 @@ public class MainActivity extends AppCompatActivity {
                // web.start();
             }
         });
+    }
+    public String md5(String s) {
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuffer hexString = new StringBuffer();
+            for (int i=0; i<messageDigest.length; i++)
+                hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
